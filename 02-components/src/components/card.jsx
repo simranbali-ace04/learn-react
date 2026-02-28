4179613591861c8e19d1d3aa09c8f101bfaf6ae6
Ace <simranbali2006@gmail.com>
2026-02-28 23:56:14 +0530

    Card using components only

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return (
+    <div className="card">
+        <h1>Merchant Navy Update</h1>
+        <p>Someone is preparing transit from Deck Cadet to 3rd Officer!  </p>
+    </div>
+    )
+}
+
+export default Card;
---
+++ 02-components/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <h3>DeckTrack Portal</h3>
+        <h5>Tracking your journey at sea.</h5>
+    </div>
+  )
+}
+
+export default Navbar
