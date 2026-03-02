8565b342056d5c367df5fb01943ef885a3c97e14
Ace <simranbali2006@gmail.com>
2026-03-02 17:53:42 +0530

    Cards using components and props

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -4,7 +4,9 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card/>
+      <Card user="NewUser" age={20} img='https://images.unsplash.com/photo-1516222338250-863216ce01ea?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvb2x8ZW58MHx8MHx8fDA%3D'/>
+      <Card user="AnotherUser" age={28} img='https://images.unsplash.com/photo-1606041008023-472dfb5e530f?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+      <Card user="OneMoreUser" age={23} img='https://images.unsplash.com/photo-1531875506263-dfcc69e73475?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNvb2x8ZW58MHx8MHx8fDA%3D'/>
     </div>
   )
 }
--- 03-props/src/components/Card.jsx
+++ 03-props/src/components/Card.jsx
@@ -6,10 +6,9 @@ const Card = (props) => {
   return (
     <div className='parent'>
     <div className="card">
-      <img src="https://images.unsplash.com/photo-1606041008023-472dfb5e530f?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-      <h1>Simran Bali</h1>
-      <p>Focused on fundamentals, driven by long-term vision.</p>
-      <button>View Profile</button>
+      <img src={props.img} alt="User" />
+      <h1>{props.user}, {props.age}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     </div>
   )
@@ -17,4 +16,3 @@ const Card = (props) => {
 
 export default Card
 
- 
