bbc0865b0fb330920bbfc48690cfc8617e41568d
Ace <simranbali2006@gmail.com>
2026-04-05 22:51:15 +0530

    Array Destructuring

--- 10-advanced-useState/src/App.jsx
+++ 10-advanced-useState/src/App.jsx
@@ -3,19 +3,20 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [num, setNum] = useState({user: 'Sam', age: 18})
+  const [num, setNum] = useState([10,20,30])
 
   const btnClicked = () => {
-    const newNum = {...num};
-    newNum.user= 'Sim'
+    const newNum = [...num];
+    newNum.push(40);
+    setNum(newNum);
     console.log(newNum);
-    setNum(newNum)
+    
     
   }
 
   return (
     <div>
-      <h1>{num.user}, {num.age}</h1>
+      <h1>{num}</h1>
       <button onClick={btnClicked}>Click</button>
     </div>
   )
