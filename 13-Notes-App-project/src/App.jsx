bccb756900860c28dea08c5fce33ec9f3f6aa841
Simran Bali <simranbali2006@gmail.com>
2026-05-02 22:18:06 +0530

    Responsiveness added

--- 13-Notes-App-project/src/App.jsx
+++ 13-Notes-App-project/src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="bg-rose-200 w-screen h-screen p-10 font-semibold gap-10">
-      <form>
-        <div className="bg-white rounded-xl p-6 shadow-xl w-1/2 flex flex-col gap-4">
+    <div className="bg-rose-200 w-screen min-h-screen md:p-10 p-10 font-semibold flex flex-col lg:flex-row gap-10">
+      <form className="w-full lg:w-1/3 sticky top-10 self-start ">
+        <div className="bg-white rounded-xl p-6 shadow-xl flex flex-col gap-4">
           <h1 className="text-2xl">Create New Note</h1>
           <input
             type="text"
@@ -20,10 +20,28 @@ const App = () => {
           </button>
         </div>
       </form>
-      <div className="flex flex-wrap p-10">
-      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl">
-
-      </div>
+      <div className=" flex-1 flex flex-wrap gap-4">
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
+      <div className="bg-white h-50 w-50 rounded-xl border-2 border-rose-300 shadow-2xl"></div>
       </div>
     </div>
   );
