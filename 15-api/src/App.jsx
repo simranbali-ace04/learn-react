56b9176697f6988199a47c7f13e27472c956d1fb
Simran Bali <simranbali2006@gmail.com>
2026-05-31 13:35:25 +0530

    API using fetch

--- 15-api/src/App.jsx
+++ 15-api/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const App = () => {
+    const getData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+  }
   return (
-    <div>
-      App
+    <div className='p-10'>
+      <button className='bg-blue-400 hover:bg-blue-700 rounded text-white px-2 py-1.5' onClick={getData}> Get Data </button>
     </div>
   )
 }
