402c21ae98d1d71ee0317cdbd92d1f22aef5fb2a
Simran Bali <simranbali2006@gmail.com>
2026-05-31 21:31:19 +0530

    axios api data accessing

--- 15-api/src/App.jsx
+++ 15-api/src/App.jsx
@@ -1,15 +1,24 @@
 import axios from 'axios'
+import {useState} from 'react'
+
 
 const App = () => {
+
+  const [data , setData] = useState([])
+  
     const getData = async () => {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      const data = response.data;
-      console.log(data);
+      const response = await axios.get('https://picsum.photos/v2/list')
+      setData(response.data);
       
   }
   return (
     <div className='p-10'>
       <button className='bg-blue-400 hover:bg-blue-700 rounded text-white px-2 py-1.5' onClick={getData}> Get Data </button>
+       <div>
+        {data.map(function(elem, idx){
+          return <h3>{idx}:Hellooo, {elem.author}</h3>
+        })}
+       </div>
     </div>
   )
 }
