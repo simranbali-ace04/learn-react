cc975bcd4b1c30a56c20d4bfe3b1e6d109b35695
Simran Bali <simranbali2006@gmail.com>
2026-05-31 14:15:04 +0530

    API using axios

--- 15-api/src/App.jsx
+++ 15-api/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import axios from 'axios'
 
 const App = () => {
     const getData = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await response.json()
-    console.log(data)
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const data = response.data;
+      console.log(data);
+      
   }
   return (
     <div className='p-10'>
