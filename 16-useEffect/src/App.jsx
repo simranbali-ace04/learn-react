88bd4521fc62b7785b8e11e3fc6361437979acc0
Simran Bali <simranbali2006@gmail.com>
2026-06-01 12:23:43 +0530

    Dependency in useEffect

--- 16-useEffect/src/App.jsx
+++ 16-useEffect/src/App.jsx
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [num, setNum]= useState(0)
+  const [num2, setNum2] = useState(100)
   useEffect(function (){
     console.log("useEffect is running...");
-  }, [])   // this will run only once when the component is mounted, because of the empty dependency array
+  }, [num2])   // this will run when num2 changes
   return (
     <div className='p-10'>
       <h1 className='text-5xl p-10'>{num}</h1>
+      <h1 className='text-5xl p-10'>{num2}</h1>
       <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded" 
-      onClick={()=>{
+      onMouseEnter={()=>{
         setNum(num+1)
-      }}>Increment</button>
+      }}
+      onMouseLeave={()=>{
+        setNum2(num2+10)
+      }}>Click</button>
     </div>
   )
 }
