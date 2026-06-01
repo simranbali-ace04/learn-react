f96a04b08e4458498892fc1501595eec6a2e48e2
Simran Bali <simranbali2006@gmail.com>
2026-06-01 13:46:30 +0530

    useEffect finished

--- 16-useEffect/src/App.jsx
+++ 16-useEffect/src/App.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [num, setNum]= useState(0)
-  const [num2, setNum2] = useState(100)
-  useEffect(function (){
-    console.log("useEffect is running...");
-  }, [num2])   // this will run when num2 changes
+
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+
+  function aChanging(){
+    console.log('A is changing');
+  }
+
+  function bChanging(){
+    console.log('B is changing');
+  }
+
+  useEffect(function(){
+    aChanging()
+    console.log('useEffect is running');
+  }, [b])
+
   return (
-    <div className='p-10'>
-      <h1 className='text-5xl p-10'>{num}</h1>
-      <h1 className='text-5xl p-10'>{num2}</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded" 
-      onMouseEnter={()=>{
-        setNum(num+1)
-      }}
-      onMouseLeave={()=>{
-        setNum2(num2+10)
-      }}>Click</button>
+    <div className='p-10 text-2xl'>
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button className='bg-blue-400 rounded p-2 m-2' onClick={()=>{
+        setA(a+1)
+      }}>Change A</button>
+      <button className='bg-blue-400 rounded p-2 m-2' onClick={()=>{
+        setB(b-1)
+      }}>Change B</button>
     </div>
   )
 }
