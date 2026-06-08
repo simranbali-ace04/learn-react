995c08f53a2fc6d42536a5c45efe0731b18365ed
Simran Bali <simranbali2006@gmail.com>
2026-06-08 06:24:03 +0530

    dynamic routing done

--- 19-routing-advance/src/pages/CourseDetails.jsx
+++ 19-routing-advance/src/pages/CourseDetails.jsx
@@ -4,12 +4,10 @@ import { useParams } from 'react-router-dom'
 const CourseDetails = () => {
 
   const params = useParams (); {/* Hook that grabs whatever is currently in the dynamic routing segment and turns it into a JS object */}
-  console.log(params);
   
-
   return (
     <div>
-      <h1>Course Details Page</h1>
+      <h1>{params.CourseId} Course Details Page</h1>
     </div>
   )
 }
